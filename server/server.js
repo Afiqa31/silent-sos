1611580506cd4101b8c1f635afcd4d3455cedda4
Afiqa31 <fathimaafiqa000@gmail.com>
2026-07-25 20:10:25 +0530

    Fix CORS for Render deployment

--- server/server.js
+++ server/server.js
@@ -26,7 +26,10 @@ connectDB();
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: [
+            "http://localhost:5173",
+            "https://silent-sos-1.onrender.com"
+        ],
         credentials: true
     })
 );
